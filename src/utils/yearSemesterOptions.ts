c7ea234eeb153420d869a3b52627a8ac627199c4
Junha Park <tunatone.0111@gmail.com>
2022-02-05 05:53:00 +0900

    [fix] fix year & semester options

--- src/utils/yearSemesterOptions.ts
+++ src/utils/yearSemesterOptions.ts
@@ -1,12 +1,12 @@
-export const yearOptions: Option[] = [
-  { label: "전체", value: "0" },
-  { label: "2022", value: "2022" },
+export const yearOptions: Option<number>[] = [
+  { label: "전체", value: 0 },
+  { label: "2022", value: 2022 },
 ];
 
-export const semesterOptions: Option[] = [
+export const semesterOptions: Option<Semester | "">[] = [
   { label: "전체", value: "" },
-  { label: "1학기", value: "spring" },
-  { label: "2학기", value: "fall" },
-  { label: "여름학기", value: "summer" },
-  { label: "겨울학기", value: "winter" },
+  { label: "1학기", value: "SPRING" },
+  { label: "2학기", value: "FALL" },
+  { label: "여름학기", value: "SUMMER" },
+  { label: "겨울학기", value: "WINTER" },
 ];
