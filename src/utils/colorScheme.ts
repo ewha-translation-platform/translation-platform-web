437654a79a71815af27a6ef0d3f8f81adb7220da
Junha Park <tunatone.0111@gmail.com>
2022-02-06 02:42:34 +0900

    [style] change danger feedback color

--- src/utils/colorScheme.ts
+++ src/utils/colorScheme.ts
@@ -2,7 +2,7 @@ import chroma from "chroma-js";
 
 function colorScheme(key: number | "default" | "danger") {
   if (key === "default") return "#00000050";
-  if (key === "danger") return "#ff0000c0";
+  if (key === "danger") return "#ff000080";
   return chroma.brewer.Set2[key % 10];
 }
 
