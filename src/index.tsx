25bb22aee2a1ed70c6bdd176d592311a394dbc53
Junha Park <tunatone.0111@gmail.com>
2022-03-19 23:39:12 +0900

    [chore] remove sentry integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
-
-if (process.env.NODE_ENV === "production") {
-  Sentry.init({
-    dsn: "https://ce8bbc53f72e42bb9c33520ba9744343@o1118404.ingest.sentry.io/6152294",
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0,
-  });
-}
 
 ReactDOM.render(
   <React.StrictMode>
