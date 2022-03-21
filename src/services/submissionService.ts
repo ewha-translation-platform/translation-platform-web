a5178cf92a41fcc6e75accc101e87f88230e2ecd
Junha Park <tunatone.0111@gmail.com>
2022-03-21 18:43:30 +0900

    [fix] fix submission service to get audioFile

--- src/services/submissionService.ts
+++ src/services/submissionService.ts
@@ -16,12 +16,21 @@ const submissionService = {
     if (submission.assignment.assignmentType === "TRANSLATION")
       return { ...submission, audioFile: null };
 
-    const audioFile = await httpService
-      .get<Blob>(`submissions/${id}/audio`, { responseType: "blob" })
-      .then(({ data }) => data)
-      .catch(() => null);
+    const { data: audioFile } = await httpService.get<Blob>(
+      `assignments/${submission.assignment.id}/audio`,
+      { responseType: "blob" }
+    );
 
-    return { ...submission, audioFile };
+    const { data: submissionAudio } = await httpService.get<Blob>(
+      `submissions/${id}/audio`,
+      { responseType: "blob" }
+    );
+
+    return {
+      ...submission,
+      assignment: { ...submission.assignment, audioFile },
+      audioFile: submissionAudio,
+    };
   },
 
   async postOne({ audioFile, ...createSubmissionDto }: CreateSubmissionDto) {
