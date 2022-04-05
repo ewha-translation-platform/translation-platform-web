29decf0b436576c82815ac654d3ae1ae6fd92b08
Junha Park <tunatone.0111@gmail.com>
2022-04-05 17:34:14 +0900

    [style] change color scheme

--- src/utils/colorScheme.ts
+++ src/utils/colorScheme.ts
@@ -3,5 +3,5 @@ import chroma from "chroma-js";
 export function colorScheme(key: number | "default" | "danger") {
   if (key === "default") return "#00000050";
   if (key === "danger") return "#ff000080";
-  return chroma.brewer.Set2[key % 10];
+  return chroma.brewer.Set3[key % 12];
 }
